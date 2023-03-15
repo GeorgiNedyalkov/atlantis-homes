5771dcb8fa9d8031327ea0be17631a91dedd3cbc
Flu3nt <gnedyalkov94@gmail.com>
2023-03-15 13:41:01 +0200

    Init repo, remove boilerplate code and add assets

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import hero from "./assets/Exterior/hero.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Atlantis Homes 3</h1>
+      <div className="hero__img">
+        <img src={hero} alt="" />
+      </div>
+      <h2>heading 2</h2>
+      <h3>heading 3</h3>
+      <h4>heading 4</h4>
+      <h5>heading 5</h5>
+      <p>Hello there homie</p>
+    </div>
+  );
+}
+
+export default App;
